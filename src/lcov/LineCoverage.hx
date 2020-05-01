5d6928e0ddcb373691fb3cf94fbdf326e9260e89
Cédric Belin <cedric@belin.io>
2020-05-01 17:34:50 +0200

    Added the line coverage classes

---
+++ src/lcov/LineCoverage.hx
@@ -0,0 +1,60 @@
+package lcov;
+
+import haxe.DynamicAccess;
+
+/** Provides the coverage data of lines. **/
+class LineCoverage {
+
+  /** The coverage data. **/
+  var data: Array<LineData>;
+
+  /** The number of lines found. **/
+  var found: Int;
+
+  /** The number of lines hit. **/
+  var hit: Int;
+
+  /**
+    Creates a new line coverage.
+    @param found The number of lines found.
+    @param hit The number of lines found.
+    @param data The coverage data.
+  **/
+  public function new(found: Int = 0, hit: Int = 0, ?data: Array<LineData>) {
+    this.data = data != null ? data : [];
+    this.found = found;
+    this.hit = hit;
+  }
+
+  /**
+    Creates a new line coverage from the specified JSON object.
+    @param map A JSON object representing a line coverage.
+    @return The instance corresponding to the specified JSON object.
+  **/
+  public static function fromJson(map: DynamicAccess<Dynamic>) return new LineCoverage(
+    Std.is(map['found'], Int) ? map['found'] : 0,
+    Std.is(map['hit'], Int) ? map['hit'] : 0,
+    Std.is(map['data'], Array) ? map['data'].map(item -> LineData.fromJson(item)) : []
+  );
+
+  /**
+    Converts this object to a map in JSON format.
+    @return The map in JSON format corresponding to this object.
+  **/
+  public function toJSON() return {
+    data: data.map(item -> item.toJSON()),
+    found: found,
+    hit: hit
+  };
+
+  /**
+    Returns a string representation of this object.
+    @return The string representation of this object.
+  **/
+  public function toString(): String {
+    final lines = data.map(item -> item.toString());
+    lines.push('${Token.linesFound}:$found');
+    lines.push('${Token.linesHit}:$hit');
+    return lines.join('\n');
+  }
+}
---
+++ src/lcov/LineData.hx
@@ -0,0 +1,58 @@
+package lcov;
+
+import haxe.DynamicAccess;
+
+/** Provides details for line coverage. **/
+class LineData {
+
+  /** The data checksum. **/
+  var checksum: String;
+
+  /** The execution count. **/
+  var executionCount: Int;
+
+  /** The line number. **/
+  var lineNumber: Int;
+
+  /**
+    Creates a new line data.
+    @param lineNumber The line number.
+    @param executionCount The execution count.
+    @param checksum The data checksum.
+  **/
+  public function new(lineNumber: Int, executionCount: Int = 0, checksum: String = '') {
+    this.checksum = checksum;
+    this.executionCount = executionCount;
+    this.lineNumber = lineNumber;
+  }
+
+  /**
+    Creates a new line data from the specified JSON object.
+    @param map A JSON object representing a line data.
+    @return The instance corresponding to the specified JSON object.
+  **/
+  public static function fromJson(map: DynamicAccess<Dynamic>) return new LineData(
+    Std.is(map['lineNumber'], Int) ? map['lineNumber'] : 0,
+    Std.is(map['executionCount'], Int) ? map['executionCount'] : 0,
+    Std.is(map['checksum'], String) ? map['checksum'] : ''
+  );
+
+  /**
+    Converts this object to a map in JSON format.
+    @return The map in JSON format corresponding to this object.
+  **/
+  public function toJSON() return {
+    checksum: checksum,
+    executionCount: executionCount,
+    lineNumber: lineNumber
+  };
+
+  /**
+    Returns a string representation of this object.
+    @return The string representation of this object.
+  **/
+  public function toString(): String {
+    final value = '${Token.lineData}:$lineNumber,$executionCount';
+    return checksum.length > 0 ? '$value,$checksum' : value;
+  }
+}
