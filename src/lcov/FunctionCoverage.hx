3034648679576fc5cdeecaec5a03b6a118db132c
Cédric Belin <cedric@belin.io>
2020-05-01 17:34:42 +0200

    Added the function coverage classes

---
+++ src/lcov/FunctionCoverage.hx
@@ -0,0 +1,61 @@
+package lcov;
+
+import haxe.DynamicAccess;
+
+/** Provides the coverage data of functions. **/
+class FunctionCoverage {
+
+  /** The coverage data. **/
+  var data: Array<FunctionData>;
+
+  /** The number of functions found. **/
+  var found: Int;
+
+  /** The number of functions hit. **/
+  var hit: Int;
+
+  /**
+    Creates a new function coverage.
+    @param found The number of functions found.
+    @param hit The number of functions found.
+    @param data The coverage data.
+  **/
+  public function new(found: Int = 0, hit: Int = 0, ?data: Array<FunctionData>) {
+    this.data = data != null ? data : [];
+    this.found = found;
+    this.hit = hit;
+  }
+
+  /**
+    Creates a new function coverage from the specified JSON object.
+    @param map A JSON object representing a function coverage.
+    @return The instance corresponding to the specified JSON object.
+  **/
+  public static function fromJson(map: DynamicAccess<Dynamic>) return new FunctionCoverage(
+    Std.is(map['found'], Int) ? map['found'] : 0,
+    Std.is(map['hit'], Int) ? map['hit'] : 0,
+    Std.is(map['data'], Array) ? map['data'].map(item -> FunctionData.fromJson(item)) : []
+  );
+
+  /**
+    Converts this object to a map in JSON format.
+    @return The map in JSON format corresponding to this object.
+  **/
+  public function toJSON() return {
+    data: data.map(item -> item.toJSON()),
+    found: found,
+    hit: hit
+  };
+
+  /**
+    Returns a string representation of this object.
+    @return The string representation of this object.
+  **/
+  public function toString(): String {
+    final lines = data.map(item -> item.toString(true));
+    for (line in data.map(item -> item.toString(false))) lines.push(line);
+    lines.push('${Token.functionsFound}:$found');
+    lines.push('${Token.functionsHit}:$hit');
+    return lines.join('\n');
+  }
+}
---
+++ src/lcov/FunctionData.hx
@@ -0,0 +1,60 @@
+package lcov;
+
+import haxe.DynamicAccess;
+
+/** Provides details for function coverage. **/
+class FunctionData {
+
+  /** The execution count. **/
+  var executionCount: Int;
+
+  /** The function name. **/
+  var functionName: String;
+
+  /** The line number of the function start. **/
+  var lineNumber: Int;
+
+  /**
+    Creates a new function data.
+    @param functionName The function name.
+    @param lineNumber The line number of the function start.
+    @param executionCount The execution count.
+  **/
+  public function new(functionName: String, lineNumber: Int, executionCount: Int = 0) {
+    this.executionCount = executionCount;
+    this.functionName = functionName;
+    this.lineNumber = lineNumber;
+  }
+
+  /**
+    Creates a new function data from the specified JSON object.
+    @param map A JSON object representing a function data.
+    @return The instance corresponding to the specified JSON object.
+  **/
+  public static function fromJson(map: DynamicAccess<Dynamic>) return new FunctionData(
+    Std.is(map['functionName'], String) ? map['functionName'] : '',
+    Std.is(map['lineNumber'], Int) ? map['lineNumber'] : 0,
+    Std.is(map['executionCount'], Int) ? map['executionCount'] : 0
+  );
+
+  /**
+    Converts this object to a map in JSON format.
+    @return The map in JSON format corresponding to this object.
+  **/
+  public function toJSON() return {
+    executionCount: executionCount,
+    functionName: functionName,
+    lineNumber: lineNumber
+  };
+
+  /**
+    Returns a string representation of this object.
+    @param asDefinition Whether to return the function definition (e.g. name and line number) instead of its data (e.g. name and execution count).
+    @return The string representation of this object.
+  **/
+  toString(asDefinition: Bool = false): String {
+    final token = asDefinition ? Token.functionName : Token.functionData;
+    final count = asDefinition ? lineNumber : executionCount;
+    return '$token:$count,$functionName';
+  }
+}
