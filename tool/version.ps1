65224bcc76ebc5defacd0e3e34b888413fa667cb
Cédric Belin <cedric@belin.io>
2020-05-03 02:36:39 +0200

    Updated the "version" task

--- tool/version.ps1
+++ tool/version.ps1
@@ -1,4 +1,17 @@
 #!/usr/bin/env pwsh
 $root = Split-Path $PSScriptRoot
 $package = Get-Content "$root/haxelib.json" | ConvertFrom-Json
-(Get-Content "$root/package.json" -Encoding UTF8) -replace '"version": "\d+(\.\d+){2}"', """version"": ""$($package.version)""" | Out-File "$root/package.json" -Encoding UTF8
+
+function Update-File {
+  param (
+    [Parameter(Mandatory = $true, Position = 0)] [String] $file,
+    [Parameter(Mandatory = $true, Position = 1)] [String] $pattern,
+    [Parameter(Mandatory = $true, Position = 2)] [String] $replacement
+  )
+
+  (Get-Content $file -Encoding UTF8) -replace $pattern, $replacement | Out-File $file -Encoding UTF8
+}
+
+Update-File "$root/package.json" '"version": "\d+(\.\d+){2}"' """version"": ""$($package.version)"""
+Update-File "$root/doc/index.md" 'release-v\d+(\.\d+){2}"' "release-v$($package.version)"
+Update-File "$root/README.md" 'release-v\d+(\.\d+){2}"' "release-v$($package.version)"
