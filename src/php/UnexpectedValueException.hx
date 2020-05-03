4c1d4c3c26ba1dd8deb7e84ac1ec506e04b70b93
Cédric Belin <cedric@belin.io>
2020-05-04 01:11:27 +0200

    Added the extern of the `UnexpectedValueException` class

---
+++ src/php/UnexpectedValueException.hx
@@ -0,0 +1,9 @@
+package php;
+
+/**
+  Exception thrown if a value does not match with a set of values.
+  Typically this happens when a function calls another function and expects the return value
+  to be of a certain type or value not including arithmetic or buffer related errors.
+**/
+@:native('UnexpectedValueException')
+extern class UnexpectedValueException extends RuntimeException {}
