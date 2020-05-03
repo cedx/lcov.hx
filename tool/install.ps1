d38a68afdb678632802c2aaf44f69fc2eb71b173
Cédric Belin <cedric@belin.io>
2020-05-03 17:02:20 +0200

    Updated the build system

--- tool/install.ps1
+++ tool/install.ps1
@@ -1,7 +1,11 @@
 #!/usr/bin/env pwsh
 Set-Location (Split-Path $PSScriptRoot)
+
 haxelib newrepo
 haxelib install checkstyle
 haxelib install dox
 haxelib install formatter
 haxelib install all
+
+composer install
+npm install --ignore-scripts --production=false
