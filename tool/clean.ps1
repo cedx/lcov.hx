d7a62a75b416d8695f114dba27e20cad5902bccf
Cédric Belin <cedric@belin.io>
2020-05-03 22:54:23 +0200

    Code optimization

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -3,4 +3,4 @@ Set-Location (Split-Path $PSScriptRoot)
 if (Test-Path lib/js/lcov.cjs) { Remove-Item lib/js/lcov.cjs }
 if (Test-Path lib/php) { Remove-Item lib/php -Recurse }
 if (Test-Path www) { Remove-Item www -Recurse }
-Get-ChildItem var -Exclude .gitkeep | ForEach-Object { Remove-Item $_.FullName -Recurse }
+foreach ($item in Get-ChildItem var -Exclude .gitkeep) { Remove-Item $item -Recurse }
