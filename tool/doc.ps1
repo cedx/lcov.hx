3e3df7076b588908a13847f8b794cc33fe805b02
Cédric Belin <cedric@belin.io>
2020-05-04 17:44:02 +0200

    Updated the documentation task

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
 Set-Location (Split-Path $PSScriptRoot)
-haxelib run dox
+haxelib run dox --input-path var --output-path doc/api --title 'LCOV Reports' --toplevel-package lcov
 mkdocs build --config-file=etc/mkdocs.yaml
