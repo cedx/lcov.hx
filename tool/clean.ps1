331bf21d08ee2b2edb4502a8e1e6a9f87d18acad
Cédric Belin <cedric@belin.io>
2020-05-04 16:52:39 +0200

    Updated the "clean" task

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 Set-Location (Split-Path $PSScriptRoot)
 foreach ($item in Get-ChildItem var -Exclude .gitkeep) { Remove-Item $item -Recurse }
-foreach ($item in @('build', 'doc/api', 'lib/js/lcov.cjs', 'lib/php', 'www')) {
+foreach ($item in @('build', 'doc/api', 'lib/js/*.cjs', 'lib/neko', 'lib/php', 'www')) {
   if (Test-Path $item) { Remove-Item $item -Recurse }
 }
