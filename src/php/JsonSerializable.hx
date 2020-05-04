a7cf5f0489d54029b4b2c20568f001e0ce28df37
Cédric Belin <cedric@belin.io>
2020-05-04 19:16:24 +0200

    Updated the documentation

--- src/php/JsonSerializable.hx
+++ src/php/JsonSerializable.hx
@@ -4,9 +4,6 @@ package php;
 @:native('JsonSerializable')
 extern interface JsonSerializable {
 
-  /**
-    Serializes the object to a value that can be serialized natively by `json_encode()`.
-    @return Data which can be serialized by `json_encode()`, which is a value of any type other than a resource.
-  **/
+  /** Serializes this object to a value that can be serialized natively by `json_encode()`. **/
 	function jsonSerialize(): Dynamic;
 }
--- src/php/UnexpectedValueException.hx
+++ src/php/UnexpectedValueException.hx
@@ -1,9 +1,5 @@
 package php;
 
-/**
-  Exception thrown if a value does not match with a set of values.
-  Typically this happens when a function calls another function and expects the return value
-  to be of a certain type or value not including arithmetic or buffer related errors.
-**/
+/** Exception thrown if a value does not match with a set of values. **/
 @:native('UnexpectedValueException')
 extern class UnexpectedValueException extends RuntimeException {}
