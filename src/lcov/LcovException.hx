1233659667fe0127cce02188330cf2aa206e97a6
Cédric Belin <cedric@belin.io>
2020-05-21 02:43:03 +0200

    The `LcovException` class now extends from `haxe.Exception`

--- src/lcov/LcovException.hx
+++ src/lcov/LcovException.hx
@@ -1,13 +1,10 @@
 package lcov;
 
+import haxe.Exception;
+
 /** An exception caused by a parsing error. **/
 @:expose
-class LcovException #if js extends js.lib.Error.SyntaxError #elseif php extends php.UnexpectedValueException #end {
-
-	#if (!js && !php)
-	/** A message describing the error. **/
-	public final message: String;
-	#end
+class LcovException extends Exception {
 
 	/** The offset in `source` where the error was detected. **/
 	public final offset: Int;
@@ -16,16 +13,8 @@ class LcovException #if js extends js.lib.Error.SyntaxError #elseif php extends 
 	public final source: String;
 
 	/** Creates a new LCOV exception. **/
-	public function new(message: String = "", source: String = "", offset: Int = 0) {
-		#if js
-		super(message);
-		name = "LcovException";
-		#elseif php
-		super(message);
-		#else
-		this.message = message;
-		#end
-
+	public function new(message: String = "", source: String = "", offset: Int = 0, ?previous: Exception) {
+		super(message, previous);
 		this.offset = offset;
 		this.source = source;
 	}
--- src/php/UnexpectedValueException.hx
+++
@@ -1,5 +0,0 @@
-package php;
-
-/** Exception thrown if a value does not match with a set of values. **/
-@:native("UnexpectedValueException")
-extern class UnexpectedValueException extends RuntimeException {}
