373f7a63fcbd9400dfc540c2945029310156d8c2
Cédric Belin <cedric@belin.io>
2020-05-23 02:02:42 +0200

    Updated the documentation

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -3,7 +3,7 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 $version = (Get-Content haxelib.json | ConvertFrom-Json).version
-haxe --define doc-gen --xml var/api.xml build.hxml
+haxe build.xml
 haxelib run dox `
 	--define description "Parse and format to LCOV your code coverage reports, in Haxe, JavaScript and PHP. The best way to share your code coverage stats." `
 	--define logo "https://api.belin.io/lcov.hx/favicon.ico" `
