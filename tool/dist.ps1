91bbaeb425f5dd7ad75c64fa313be6054bfbdbfe
Cédric Belin <cedric@belin.io>
2020-05-24 01:26:21 +0200

    Fixed the "dist" task on Linux

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -8,7 +8,7 @@ haxe --no-traces build.hxml
 
 if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
 Copy-Item lib/js/lcov.js build/lcov.js
-node_modules/.bin/terser.ps1 --config-file=etc/terser.json --output=build/lcov.min.js build/lcov.js
+node_modules/.bin/terser --config-file=etc/terser.json --output=build/lcov.min.js build/lcov.js
 
 foreach ($item in Get-ChildItem lib/php) {
 	if ($item.BaseName -ne "lcov") { Remove-Item $item -Recurse }
