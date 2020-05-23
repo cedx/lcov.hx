72d30d21af752a8b0881c0db32ad48b9b730cc5d
Cédric Belin <cedric@belin.io>
2020-05-23 22:49:17 +0200

    Code formatting

--- src/lcov/LcovException.hx
+++ src/lcov/LcovException.hx
@@ -3,8 +3,7 @@ package lcov;
 import haxe.Exception;
 
 /** An exception caused by a parsing error. **/
-@:expose
-class LcovException extends Exception {
+@:expose class LcovException extends Exception {
 
 	/** The offset in `source` where the error was detected. **/
 	public final offset: Int;
