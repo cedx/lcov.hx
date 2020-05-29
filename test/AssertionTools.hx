022014c2d264977910eb589a1741cd64dc8f600a
Cédric Belin <cedric@belin.io>
2020-05-29 10:40:32 +0200

    The exception class parameter is optional

--- test/AssertionTools.hx
+++ test/AssertionTools.hx
@@ -5,14 +5,14 @@ import tink.unit.AssertionBuffer;
 class AssertionTools {
 
 	/** Expects the function `func` to throw an error. **/
-	public static function throws(asserts: AssertionBuffer, func: () -> Void, exceptionClass: Class<Exception>): Void {
+	public static function throws(asserts: AssertionBuffer, func: () -> Void, ?exceptionClass: Class<Exception>): Void {
 		try {
 			func();
 			asserts.fail("Exception not thrown");
 		}
 
 		catch (e) {
-			asserts.assert(Std.isOfType(e, exceptionClass));
+			asserts.assert(Std.isOfType(e, exceptionClass != null ? exceptionClass : Exception));
 		}
 	}
 }
