b6185f3c63504ce349411b2540b0fcad52f23fb9
Cédric Belin <cedric@belin.io>
2020-06-06 01:52:44 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -10,6 +10,4 @@ if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
 Copy-Item lib/js/lcov.js build/lcov.js
 node_modules/.bin/terser --config-file=etc/terser.json --output=build/lcov.min.js build/lcov.js
 
-foreach ($item in Get-ChildItem lib/php) {
-	if ($item.Name -ne "lcov") { Remove-Item $item -Recurse }
-}
+Get-ChildItem lib/php -Exclude lcov | Remove-Item -Recurse
