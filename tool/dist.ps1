75677d5728a78ae55e6cd43590449a2acf03ead0
Cédric Belin <cedric@belin.io>
2020-06-08 20:41:36 +0200

    Updated the build system

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -8,6 +8,6 @@ tool/build.ps1
 
 if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
 Copy-Item lib/js/lcov.js build/lcov.js
-node_modules/.bin/terser --config-file=etc/terser.json --output=build/lcov.min.js build/lcov.js
+npm run dist
 
 Get-ChildItem lib/php -Exclude lcov | Remove-Item -Recurse
