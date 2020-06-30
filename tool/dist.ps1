1664e20756362543c36faea0e1dd175e914771f3
Cédric Belin <cedric@belin.io>
2020-06-30 18:10:15 +0200

    Fixed a regressions

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -10,4 +10,4 @@ if (-not (Test-Path build)) { New-Item build -ItemType Directory | Out-Null }
 Copy-Item lib/js/lcov.js build/lcov.js
 npm run dist
 
-Remove-Item lib/php/* -Exclude lcov -Force -Recurse
+Get-ChildItem lib/php -Exclude lcov | Remove-Item -Force -Recurse
