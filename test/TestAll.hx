d0e26b95989c13de1ee563d8eb923b02ea043632
Cédric Belin <cedric@belin.io>
2020-12-07 19:19:06 +0100

    Fixed the CI

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -3,11 +3,18 @@ import lcov.*;
 import tink.testrunner.Runner;
 import tink.unit.TestBatch;
 
+#if php
+import php.Const;
+import php.Global.error_reporting;
+#end
+
 /** Runs the test suite. **/
 class TestAll {
 
 	/** Application entry point. **/
 	static function main() {
+		#if php if (Const.PHP_VERSION_ID >= 80000) error_reporting(Const.E_ALL & ~Const.E_DEPRECATED); #end
+
 		final tests = TestBatch.make([
 			new BranchCoverageTest(),
 			new BranchDataTest(),
