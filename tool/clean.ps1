988326f1809fbabea214116f45509650498f77a7
Cédric Belin <cedric@belin.io>
2020-12-12 01:05:15 +0100

    Updated the build system

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -2,7 +2,7 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-foreach ($item in "build", "docs/api", "lib/index.php", "lib/js/lcov.*", "lib/php", "lib/res") {
+foreach ($item in "build", "docs/api", "lib/index.php", "lib/js/bundle.*", "lib/php", "lib/res") {
 	if (Test-Path $item) { Remove-Item $item -Force -Recurse }
 }
 
