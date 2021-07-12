8dddda28fbbb7218f0f9edfb5762590a0a290c9d
Cédric Belin <cedric@belin.io>
2021-07-12 14:04:44 +0200

    Replaced the build system based on PowerShell by Lix scripts

---
+++ scripts/Doc.hx
@@ -0,0 +1,26 @@
+import Sys.*;
+import Tools.removeDirectory;
+import haxe.Json;
+import sys.FileSystem.*;
+import sys.io.File.*;
+
+/** Runs the script. **/
+function main() {
+	if (exists("docs/api")) removeDirectory("docs/api");
+
+	command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
+	command("lix", [
+		"run", "dox",
+		"--define", "description", "Parse and format to LCOV your code coverage reports, in Haxe.",
+		"--define", "source-path", "https://github.com/cedx/lcov.hx/blob/main/src",
+		"--define", "themeColor", "0xffc105",
+		"--define", "version", Json.parse(getContent("haxelib.json")).version,
+		"--define", "website", "https://github.com/cedx/lcov.hx",
+		"--input-path", "var",
+		"--output-path", "docs/api",
+		"--title", "LCOV Reports for Haxe",
+		"--toplevel-package", "lcov"
+	]);
+
+	copy("docs/favicon.ico", "docs/api/favicon.ico");
+}
--- tool/build.ps1
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
-Set-Location (Split-Path $PSScriptRoot)
-foreach ($item in "js", "php") { haxe "build_$item.hxml" }
