871d746e30e9936fb594860b4aed8ee23a7326e8
Cédric Belin <cedric@belin.io>
2021-07-12 17:45:56 +0200

    Updated the "Clean" task

--- scripts/Clean.hx
+++ scripts/Clean.hx
@@ -1,4 +1,9 @@
 import Tools.cleanDirectory;
+import Tools.removeDirectory;
+import sys.FileSystem.*;
 
 /** Runs the script. **/
-function main() cleanDirectory("var");
+function main() {
+	if (exists("lib")) removeDirectory("lib");
+	cleanDirectory("var");
+}
