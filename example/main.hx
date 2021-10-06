d2b178a5fedcd0d9fff45de3d18d0240674538b2
Cédric Belin <cedric@belin.io>
2021-10-06 21:57:29 +0200

    Updated the documentation

--- example/main.hx
+++ example/main.hx
@@ -4,15 +4,13 @@ import tink.Json;
 
 /** Formats coverage data as LCOV report. **/
 function formatReport() {
-	final lineCoverage = new LineCoverage({found: 2, hit: 2, data: [
-		new LineData({lineNumber: 6, executionCount: 2, checksum: "PF4Rz2r7RTliO9u6bZ7h6g"}),
-		new LineData({lineNumber: 7, executionCount: 2, checksum: "yGMB6FhEEAd8OyASe3Ni1w"})
-	]});
-
 	final record = new Record({
 		sourceFile: "/home/cedx/lcov.hx/fixture.hx",
 		functions: new FunctionCoverage({found: 1, hit: 1}),
-		lines: lineCoverage
+		lines: new LineCoverage({found: 2, hit: 2, data: [
+			new LineData({lineNumber: 6, executionCount: 2, checksum: "PF4Rz2r7RTliO9u6bZ7h6g"}),
+			new LineData({lineNumber: 7, executionCount: 2, checksum: "yGMB6FhEEAd8OyASe3Ni1w"})
+		]})
 	});
 
 	final report = new Report({testName: "Example", records: [record]});
