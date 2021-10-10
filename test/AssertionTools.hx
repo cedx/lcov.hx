b2d2e425ffe909ca3810597c9c55cdf224fd60dd
Cédric Belin <cedric@belin.io>
2021-10-10 13:45:29 +0200

    Updated the documentation

--- test/AssertionTools.hx
+++ test/AssertionTools.hx
@@ -4,7 +4,7 @@ import tink.unit.AssertionBuffer;
 /** Provides helper methods for handling assertions. **/
 abstract class AssertionTools {
 
-	/** Expects the function `func` to throw an exception. **/
+	/** Expects the specified function to throw an exception. **/
 	public static function throws(asserts: AssertionBuffer, func: () -> Void, ?exceptionClass: Class<Exception>)
 		try { func(); asserts.fail("Exception not thrown."); }
 		catch (e) asserts.assert(Std.isOfType(e, exceptionClass != null ? exceptionClass : Exception));
