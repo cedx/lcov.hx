f454fae88ac14a707c8ecf94f7702df754c62a01
Cédric Belin <cedric@belin.io>
2021-12-08 17:03:04 +0100

    Updated the example code

--- example/parse_report.hx
+++ example/parse_report.hx
@@ -1,11 +1,12 @@
-import lcov.LcovException;
 import lcov.Report;
 import sys.io.File;
 import tink.Json;
 
 /** Parses a LCOV report to coverage data. **/
-function main() try {
-	final report = Report.fromCoverage(File.getContent("/path/to/lcov.info"));
-	Sys.println('The coverage report contains ${report.records.length} records:');
-	Sys.println(Json.stringify(report, null, "\t"));
-} catch (e: LcovException) Sys.println('An error occurred: "${e.message}" at offset ${e.offset}.');
+function main() switch Report.fromString(File.getContent("/path/to/lcov.info")) {
+	case Failure(error):
+		Sys.println('An error occurred: "${error.message}" at offset ${error.data}.');
+	case Success(report):
+		Sys.println('The coverage report contains ${report.records.length} records:');
+		Sys.println(Json.stringify(report, null, "\t"));
+}
