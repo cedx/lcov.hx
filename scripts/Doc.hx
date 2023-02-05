6b80540600e8833b74c4b36de2aaf7fe8303cc26
Cédric Belin <cedric@belin.io>
2023-02-05 21:54:06 +0100

    Ported the build system to Lix scripts

---
+++ scripts/Doc.hx
@@ -0,0 +1,25 @@
+//! --class-path src
+import lcov.Version;
+import sys.FileSystem;
+import sys.io.File;
+
+/** Runs the script. **/
+function main() {
+	if (FileSystem.exists("docs")) Tools.removeDirectory("docs");
+
+	Sys.command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
+	Sys.command("lix", [
+		"run", "dox",
+		"--define", "description", "Parse and format to LCOV your code coverage reports, in Haxe.",
+		"--define", "source-path", "https://github.com/cedx/lcov.hx/blob/main/src",
+		"--define", "themeColor", "0xffc105",
+		"--define", "version", Version.packageVersion,
+		"--define", "website", "https://github.com/cedx/which.hx",
+		"--input-path", "var",
+		"--output-path", "docs",
+		"--title", "LCOV Reports for Haxe",
+		"--toplevel-package", "lcov"
+	]);
+
+	File.copy("www/favicon.ico", "docs/favicon.ico");
+}
