076091f2bcfc2713b0574b6ccdf89d0fbf09b304
Cédric Belin <cedric@belin.io>
2023-02-20 15:53:46 +0100

    Documented the build system [skip ci]

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -3,7 +3,7 @@ import lcov.Version;
 import sys.FileSystem;
 import sys.io.File;
 
-/** Runs the script. **/
+/** Builds the documentation. **/
 function main() {
 	if (FileSystem.exists("docs")) Tools.removeDirectory("docs");
 
