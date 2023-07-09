4f9123a1680f75ba3eb92d7afe3e94feb5d3e203
Cédric Belin <cedric@belin.io>
2023-07-09 16:53:31 +0200

    Rename the `Version` class to `Platform`

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -1,5 +1,5 @@
 //! --class-path src --library tink_core
-import lcov.Version;
+import lcov.Platform;
 import sys.FileSystem;
 import sys.io.File;
 
@@ -12,7 +12,7 @@ function main() {
 		"--define", "description", "Parse and format to LCOV your code coverage reports, in Haxe.",
 		"--define", "source-path", "https://github.com/cedx/lcov.hx/blob/main/src",
 		"--define", "themeColor", "0xea8220",
-		"--define", "version", Version.packageVersion,
+		"--define", "version", Platform.packageVersion,
 		"--define", "website", "https://docs.belin.io/lcov.hx",
 		"--input-path", "var",
 		"--output-path", "docs",
